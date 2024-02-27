4babef36de9555c615ff48eb7afbcdeceb734897
Jalen Pinder <jtp1326@gmail.com>
2024-02-27 19:23:54 -0400

    Added some JavaScript function (Income button works), added a text shadow to the logo/h1, and a day and night button to the header.

---
+++ index.js
@@ -0,0 +1,28 @@
+// Calculate Income
+var hourlyWage = document.querySelector("#income .hourly-wage input");
+var hoursWorked = document.querySelector("#income .hours-worked input");
+var incomeButton = document.querySelector("#income .calculate button");
+var incomeResult = document.querySelector("#income .calculate .total");
+
+incomeButton.addEventListener("click", function() {
+    var totalIncome = hourlyWage.valueAsNumber * hoursWorked.valueAsNumber;
+    incomeResult.textContent = "$" + totalIncome;
+});
+
+// Calculate Bills
+var billsButton = document.querySelector("#bills .calculate button");
+var billsResult = document.querySelector("#bills .calculate .total");
+
+function calculateBills() {
+    var billList = document.querySelectorAll(".bill-item-container input[type='checkbox']:checked");
+    for(var i = 0; i < billList.length; i++) {
+        
+    }
+}
+
+// Calculate Budget & Summary
+var budgetButton = document.querySelector("#budget .calculate button");;
+
+function calculateBudget() {
+    
+}
